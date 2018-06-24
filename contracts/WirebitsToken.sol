a5407cb59e32fe35909c41c79b19a06452bb701a
Michael O'Sullivan <michael@nizamimedia.com>
2018-06-24 01:49:26 +0100

    Transfer token functionality, and testing suite.

--- contracts/WirebitsToken.sol
+++ contracts/WirebitsToken.sol
@@ -20,11 +20,25 @@ contract WirebitsToken {
   //setting our global function
   uint256 public totalSupply;
 
+  event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
   //maps address integar of where the balance of any set of tokens are
   mapping(address => uint256) public balanceOf;
 
   function WirebitsToken (uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
     }
 }
