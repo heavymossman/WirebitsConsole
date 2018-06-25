a99aa943d3861df6fb898033310057c2a20badd5
Michael O'Sullivan <michael@nizamimedia.com>
2018-06-25 01:13:13 +0100

    FULL ICO SMART CONTRACT BACKEND IS DONE!!

--- test/WirebitsToken.js
+++ test/WirebitsToken.js
@@ -25,10 +25,10 @@ contract('DappToken', function(accounts) {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply) {
-      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+      assert.equal(totalSupply.toNumber(), 10000000, 'sets the total supply to 1,000,000');
       return tokenInstance.balanceOf(accounts[0]);
     }).then(function(adminBalance) {
-      assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
+      assert.equal(adminBalance.toNumber(), 10000000, 'it allocates the initial supply to the admin account');
     });
   });
 
@@ -40,22 +40,22 @@ contract('DappToken', function(accounts) {
       return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
     }).then(assert.fail).catch(function(error) {
       assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
-      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+      return tokenInstance.transfer.call(accounts[1], 2500000, { from: accounts[0] });
     }).then(function(success) {
       assert.equal(success, true, 'it returns true');
-      return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+      return tokenInstance.transfer(accounts[1], 2500000, { from: accounts[0] });
     }).then(function(receipt) {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
       assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
       assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
-      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      assert.equal(receipt.logs[0].args._value, 2500000, 'logs the transfer amount');
       return tokenInstance.balanceOf(accounts[1]);
     }).then(function(balance) {
-      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+      assert.equal(balance.toNumber(), 2500000, 'adds the amount to the receiving account');
       return tokenInstance.balanceOf(accounts[0]);
     }).then(function(balance) {
-      assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+      assert.equal(balance.toNumber(), 7500000, 'deducts the amount from the sending account');
     });
   });
 
