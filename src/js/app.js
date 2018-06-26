81ac515d4b0d37cd4a34983ddec153d08d90afdd
Michael O'Sullivan <michael@nizamimedia.com>
2018-06-26 22:05:52 +0100

    Fixed all block chain connection issues, now displaying smart contract address in console! Thank god!

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,10 @@
 App = {
+
   web3Provider: null,
   contracts: {},
-  account: '0x0',
-  loading: false,
-  tokenPrice: 10000000000000000,
-  tokensSold: 0,
-  tokensAvailable: 7500000,
 
-  init: function() {
-    console.log("App initialized...")
+  init: function(){
+    console.log("App initialised")
     return App.initWeb3();
   },
 
@@ -19,9 +15,10 @@ App = {
       web3 = new Web3(web3.currentProvider);
     } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:3000');
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545'); //The port needs to be the same as Ganache
       web3 = new Web3(App.web3Provider);
     }
+
     return App.initContracts();
   },
 
@@ -32,97 +29,7 @@ App = {
       App.contracts.WirebitsTokenSale.deployed().then(function(wirebitsTokenSale) {
         console.log("Wirebits Token Sale Address:", wirebitsTokenSale.address);
       });
-    }).done(function() {
-      $.getJSON("WirebitsToken.json", function(wirebitsToken) {
-        App.contracts.WirebitsToken = TruffleContract(wirebitsToken);
-        App.contracts.WirebitsToken.setProvider(App.web3Provider);
-        App.contracts.WirebitsToken.deployed().then(function(wirebitsToken) {
-          console.log("Wirebits Token Address:", wirebitsToken.address);
-        });
-
-        App.listenForEvents();
-        return App.render();
-      });
     })
-  },
-
-  // Listen for events emitted from the contract
-  listenForEvents: function() {
-    App.contracts.WirebitsTokenSale.deployed().then(function(instance) {
-      instance.Sell({}, {
-        fromBlock: 0,
-        toBlock: 'latest',
-      }).watch(function(error, event) {
-        console.log("event triggered", event);
-        App.render();
-      })
-    })
-  },
-
-  render: function() {
-    if (App.loading) {
-      return;
-    }
-    App.loading = true;
-
-    var loader  = $('#loader');
-    var content = $('#content');
-
-    loader.show();
-    content.hide();
-
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      if(err === null) {
-        App.account = account;
-        $('#accountAddress').html("Your Account: " + account);
-      }
-    })
-
-    // Load token sale contract
-    App.contracts.WirebitsTokenSale.deployed().then(function(instance) {
-      dappTokenSaleInstance = instance;
-      return wirebitsTokenSaleInstance.tokenPrice();
-    }).then(function(tokenPrice) {
-      App.tokenPrice = tokenPrice;
-      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
-      return wirebitsTokenSaleInstance.tokensSold();
-    }).then(function(tokensSold) {
-      App.tokensSold = tokensSold.toNumber();
-      $('.tokens-sold').html(App.tokensSold);
-      $('.tokens-available').html(App.tokensAvailable);
-
-      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
-      $('#progress').css('width', progressPercent + '%');
-
-      // Load token contract
-      App.contracts.WirebitsToken.deployed().then(function(instance) {
-        dappTokenInstance = instance;
-        return wirebitsTokenSaleInstance.balanceOf(App.account);
-      }).then(function(balance) {
-        $('.dapp-balance').html(balance.toNumber());
-        App.loading = false;
-        loader.hide();
-        content.show();
-      })
-    });
-  },
-
-  buyTokens: function() {
-    $('#content').hide();
-    $('#loader').show();
-    var numberOfTokens = $('#numberOfTokens').val();
-    App.contracts.WirebitsTokenSale.deployed().then(function(instance) {
-      return instance.buyTokens(numberOfTokens, {
-        from: App.account,
-        value: numberOfTokens * App.tokenPrice,
-        gas: 500000 // Gas limit
-      });
-    }).then(function(result) {
-      console.log("Tokens bought...")
-      $('form').trigger('reset') // reset number of tokens in form
-      // Wait for Sell event
-    });
   }
 }
 
